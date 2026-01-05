1ebdb782d21bec142a616e77f3792c28605d7f60
SavannahMoyer5023 <SavannahMoyer5023@hotmail.com>
2026-01-05 13:11:45 +0800

    feat: update API_VERSION to v2.0.0 and add log in getTime

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// 这是一个基础配置文件
-const API_VERSION = "v1.0.0";
-const IS_DEBUG = true;
-
-function initApp() {
-    console.log("App is initializing...");
-}
-
-// 这里的 time 函数是演示用的
-function getTime() {
-    return new Date().toLocaleTimeString();
-}
+后续修改后的代码（需您提供原代码才能给出）
