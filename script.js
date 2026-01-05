9cf33f1498c9aa2f4e1ce836ac6b1f831addb543
勾琦豪 <2087148166@qq.com>
2026-01-05 12:53:55 +0800

    测试案例

---
+++ script.js
@@ -0,0 +1,12 @@
+// 这是一个基础配置文件
+const API_VERSION = "v1.0.0";
+const IS_DEBUG = true;
+
+function initApp() {
+    console.log("App is initializing...");
+}
+
+// 这里的 time 函数是演示用的
+function getTime() {
+    return new Date().toLocaleTimeString();
+}
